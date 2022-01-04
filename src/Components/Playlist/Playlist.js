8894ce96da35fa758e63d36bfc3956cfb95afab1
alexander <alexander.caelan.rogers@gmail.com>
2022-01-04 16:21:54 -0500

    export and import Playlist.js

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,1 +1,15 @@
 import React from "react";
+
+class Playlist extends React.Component {
+    render() {
+        return (
+            <div class="Playlist">
+                <input defaultValue={"New Playlist"}/>
+                {/* <TrackList />*/}
+                <button class="Playlist-save">SAVE TO SPOTIFY</button>
+            </div>
+        )
+    }
+}
+
+export default Playlist;
