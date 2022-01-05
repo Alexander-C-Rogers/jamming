786fb9b2845adcc98661de48ed1a4c7d921d7a3e
alexander <alexander.caelan.rogers@gmail.com>
2022-01-05 16:23:46 -0500

    editing Spotify method

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -1,5 +1,5 @@
 const clientId = ' 5c833c43016344dc98ad03a126b10388';
-let accessToken;
+let accessToken = undefined;
 
 const Spotify = {
     getAccessToken() {
@@ -7,9 +7,16 @@ const Spotify = {
             return accessToken;
         }
 
-        //check for access token match*
-        const accessToken = window.location.href.match(/access_token=([^&]*)/);
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
     }
 }
 
