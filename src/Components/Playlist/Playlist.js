46be0157147bee89000be52f13c6542521241291
alexander <alexander.caelan.rogers@gmail.com>
2022-01-04 22:01:37 -0500

    Pass the playlist tracks from the playlist component to the TrackList component.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import './Playlist.css';
 
+import TrackList from "../TrackLList/TrackList";
+
 class Playlist extends React.Component {
     render() {
         return (
             <div class="Playlist">
-                <input defaultValue={"New Playlist"}/>
-                {/* <TrackList />*/}
+                <input defaultValue={"New Playlist"}  />
+                <TrackList tracks={this.props.playlsistTracks}  />
                 <button class="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
